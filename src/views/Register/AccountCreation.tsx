7aea5e64f6f69072672a0b16f09cc9af9a7443b8
Mateusz Suder <matisu999@gmail.com>
2021-06-15 23:31:07 +0200

    fix register

--- src/views/Register/AccountCreation.tsx
+++ src/views/Register/AccountCreation.tsx
@@ -1,7 +1,8 @@
 import { Container, CssBaseline, Avatar, Typography, Grid, TextField, Button, makeStyles } from '@material-ui/core';
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import {createUser} from "../../services/userService";
 
 const useStyles = makeStyles((theme) => ({
 	paper: {
@@ -27,6 +28,18 @@ export const AccountCreation = () => {
 	const [username, setUsername] = useState<string>("");
 	const classes = useStyles();
 
+	const history = useHistory();
+
+	const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+		setUsername(event.target.value as string);
+	};
+
+	const register = () => {
+		createUser(username).then(r =>
+			history.push('/')
+		);
+	}
+
 	return (
 		<Container component="main" maxWidth="xs">
 			<CssBaseline />
@@ -37,35 +50,36 @@ export const AccountCreation = () => {
 				<Typography component="h1" variant="h5">
 					Utwórz konto
 				</Typography>
-				<form className={classes.form} noValidate>
-					<Grid item xs={12}>
-					<TextField
-						variant="outlined"
-						required
-						fullWidth
-						id="username"
-						label="Nazwa użytkownika"
-						name="username"
-						autoComplete="username"
-					/>
+				<Grid item xs={12}>
+				<TextField
+					variant="outlined"
+					required
+					fullWidth
+					id="username"
+					label="Nazwa użytkownika"
+					name="username"
+					autoComplete="username"
+					value={username}
+					onChange={handleChange}
+				/>
+				</Grid>
+				<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					color="primary"
+					className={classes.submit}
+					onClick={register}
+				>
+					Utwórz konto
+				</Button>
+				<Grid container justify="flex-end">
+					<Grid item>
+						<Link to="/login">
+							Masz już konto? Zaloguj się
+						</Link>
 					</Grid>
-					<Button
-						type="submit"
-						fullWidth
-						variant="contained"
-						color="primary"
-						className={classes.submit}
-					>
-						Utwórz konto
-					</Button>
-					<Grid container justify="flex-end">
-						<Grid item>
-							<Link to="/login">
-								Masz już konto? Zaloguj się
-							</Link>
-						</Grid>
-					</Grid>
-				</form>
+				</Grid>
 			</div>
 		</Container>
 	);
